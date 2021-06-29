5a863948f9e1d5a7c3a8d2b981c310a3be201d49
Mwansa Puta <mwansa.puta@my.metrostate.edu>
2021-06-29 17:35:52 +0000

    initial commit

---
+++ api.py
@@ -0,0 +1,35 @@
+import requests
+import spotipy
+
+CLIENT_ID = '7d9c7a429a544319bcd98e0efa7b3104'
+CLIENT_SECRET ='1c5f121c38894affb3171ac0d724b828'
+
+AUTH_URL = 'https://accounts.spotify.com/api/token'
+
+# POST
+auth_response = requests.post(AUTH_URL, {
+    'grant_type': 'client_credentials',
+    'client_id': CLIENT_ID,
+    'client_secret': CLIENT_SECRET,
+})
+
+#  convert the response to JSON
+auth_response_data = auth_response.json()
+
+# save the access token
+access_token = auth_response_data['access_token']
+
+headers = {
+    'Authorization': 'Bearer {token}'.format(token=access_token)
+}
+
+# base URL of all Spotify API endpoints
+BASE_URL = 'https://api.spotify.com/v1/'
+
+# Track ID from the URI
+track_id = '6y0igZArWVi6Iz0rj35c1Y'
+
+# actual GET request with proper header
+r = requests.get(BASE_URL + 'audio-features/' + track_id, headers=headers)
+r = r.json()
+print(r)
